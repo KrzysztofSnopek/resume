776cb2f3f0789d3ed5ded97486dd8c962c08f058
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-01-24 22:11:48 +0100

    Fixes grid problem in Home component

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,30 +2,29 @@ import '../Styles/Home.css'
 
 export function Home() {
   return (
-      <div className='home home-grid'>
+      <div className='home home-grid' id='home-grid'>
 
         <div className='greetings'>
           <div className="left-side intro-head">
             Krzysztof Snopek
           </div>   
+
           <div className='intro-body'>
             I like to create solid apps and learn new technologies.
           </div>
-          <div className='intro-body-small home-grid'>
-            <div>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error consectetur eos quas architecto nulla ad necessitatibus illum tempore tenetur dolorem.
-            </div>
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium fugiat cumque expedita natus ipsa fugit vel sapiente. Impedit, qui ea.
-            </div>
-          </div>
+        </div>
+
+        <div className='col-1 intro-body-small'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error consectetur eos quas architecto nulla ad necessitatibus illum tempore tenetur dolorem.
+        </div>
+
+        <div className='col-2 intro-body-small'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium fugiat cumque expedita natus ipsa fugit vel sapiente. Impedit, qui ea.
         </div>
 
         <div className="img-border">
-          <div className='photo-2'>      
-          </div>          
+          <div className='photo-2'></div>          
         </div>
-
       </div>
   )
 }
