316abf71983d143b9dea4105f08949fb53d7d296
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-01-27 19:57:38 +0100

    Make image carousel in works section responsive

--- src/Styles/StyleImageSlider.tsx
+++ src/Styles/StyleImageSlider.tsx
@@ -1,27 +1,27 @@
 export const imgContainer = {
-    padding: '1rem 0',
-    margin: '0 2rem',
+    padding: '2rem',
     display: 'flex',
     justifyContent: 'center'
 }
 
 export const slide = {
   backgroundColor: 'rgb(44, 44, 165)',
-  minHeight: '500px',
-  height: '500px',
-  maxWidth: '700px',
+  maxHeight: '800px',
+  maxWidth: '800px',
+  minWidth: '200px',
+  minHeight: '200px',
   backgroundSize: 'contain',
   backgroundRepeat: 'no-repeat',
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  padding: '2rem 0',
-  borderRadius: '1rem',
+  borderRadius: '2rem',
+  padding: '2rem 0'
 }
 export const slideImage = {
-  height: '500px',
-  minHeight: '500px',
+  // height: '50*vh',
   width: '700px',
+  minHeight: `calc(35vw)`,
   backgroundSize: 'contain',
   backgroundRepeat: 'no-repeat',
   backgroundPosition: 'center',
@@ -29,5 +29,5 @@ export const slideImage = {
 export const arrow = {
   cursor: 'pointer',
   fontSize: '35px',
-  padding: '0.5rem',
+  padding: '2.5rem',
 }
