daf5857560e5f8cc6211ee3ec8dab7391d4229df
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-01-30 20:38:34 +0100

    Adds grid order to contact section

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -2,25 +2,25 @@ import '../Styles/Contact.css'
 
 export function Contact() {
   return (
-    <div className='font-bold text-3xl text-secondary px-10 bg-outro bg-contact-pattern bg-no-repeat p-2 pt-[20vh] md:pt-[30vh] lg:pt-[40vh] grid md:grid-cols-1 lg:grid-cols-2'>
-      <div className='p-8 md:order-1'>GET IN TOUCH</div>
+    <div className='font-bold md:text-3xl sm:text-xl text-secondary bg-outro bg-contact-pattern bg-no-repeat p-2 pt-[20vh] md:pt-[30vh] lg:pt-[40vh] grid md:grid-cols-1 lg:grid-cols-2'>
+      <div className='p-8 lg:order-1'>GET IN TOUCH</div>
 
-      <div className='p-8 md:order-3'>Do you think I could fit in the team? Let's make it reality. Message me now!</div>        
+      <div className='p-8 lg:order-3'>Do you think I could fit in the team? Let's make it reality. Message me now!</div>        
 
-      <div className='p-3 md:order-2 row-span-2'>
-        <div className='p-5'>
+      <div className='p-5 lg:order-2 row-span-2'>
+        <div className='p-3'>
           <div className=''>Email</div>
-          <div className=''>krzysztof.snopek1@gmail.com</div>
+          <div className='pr-2'>krzysztof.snopek1@gmail.com</div>
         </div>
 
-        <div className='p-5'>
+        <div className='p-3'>
           <div className=''>Phone</div>
           <div className=''>+48 668 698 369</div>
         </div>  
 
-         <div className='p-5'>
-          <div className=''>Github</div>
-          <a href='https://github.com/KrzysztofSnopek' className='git-link text-2xl'>https://github.com/KrzysztofSnopek</a>
+         <div className='p-3'>
+          <div>Github</div>
+          <a href='https://github.com/KrzysztofSnopek' className='git-link text-xl sm:text-xl md:text-3xl text-secondary'>https://github.com/KrzysztofSnopek</a>
         </div>              
       </div>
     </div>
