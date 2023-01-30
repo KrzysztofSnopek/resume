aa9077cad74ae3aaae4c370c8af11350d96e0541
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-01-30 20:46:56 +0100

    Adds left border to grid elements in contact section

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -2,12 +2,12 @@ import '../Styles/Contact.css'
 
 export function Contact() {
   return (
-    <div className='font-bold md:text-3xl sm:text-xl text-secondary bg-outro bg-contact-pattern bg-no-repeat p-2 pt-[20vh] md:pt-[30vh] lg:pt-[40vh] grid md:grid-cols-1 lg:grid-cols-2'>
-      <div className='p-8 lg:order-1'>GET IN TOUCH</div>
+    <div className='font-bold md:text-3xl sm:text-xl text-secondary bg-outro bg-contact-pattern bg-no-repeat p-4 pt-[20vh] md:pt-[30vh] lg:pt-[40vh] grid md:grid-cols-1 lg:grid-cols-2 gap-6'>
+      <div className='p-8 lg:order-1 border-l-4 border-l-lime-300 ml-2'>GET IN TOUCH</div>
 
-      <div className='p-8 lg:order-3'>Do you think I could fit in the team? Let's make it reality. Message me now!</div>        
+      <div className='p-8 lg:order-3 border-l-4 border-l-lime-300 ml-2'>Do you think I could fit in the team? Let's make it reality. Message me now!</div>        
 
-      <div className='p-5 lg:order-2 row-span-2'>
+      <div className='p-5 lg:order-2 row-span-2 border-l-4 border-l-lime-300 ml-2'>
         <div className='p-3'>
           <div className=''>Email</div>
           <div className='pr-2'>krzysztof.snopek1@gmail.com</div>
