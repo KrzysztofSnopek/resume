97f15175f9e668c63d3ad84de1b476c2c8bd1fab
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-02-01 19:41:08 +0100

    Adds animations to contact section

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,18 +1,34 @@
+import { useEffect } from "react";
 import "../Styles/Contact.css";
+import "aos/dist/aos.css";
+import Aos from "aos";
 
 export function Contact() {
+  useEffect(() => {
+    Aos.init({ duration: 3000 });
+  }, []);
+
   return (
     <div className="grid gap-6 bg-outro bg-contact-pattern bg-no-repeat p-4 pt-[20vh] font-bold text-secondary md:grid-cols-1 md:pt-[30vh] lg:grid-cols-2 lg:pt-[40vh]">
-      <div className="ml-2 border-l-4 border-l-lime-300 p-8 xsm:text-2xl md:text-4xl lg:order-1">
+      <div
+        className="ml-2 border-l-4 border-l-lime-300 p-8 xsm:text-2xl md:text-4xl lg:order-1"
+        data-aos="fade-left"
+      >
         GET IN TOUCH
       </div>
 
-      <div className="ml-2 max-w-sm border-l-4 border-l-lime-300 p-8 xsm:text-2xl md:text-4xl lg:order-3">
+      <div
+        className="ml-2 max-w-sm border-l-4 border-l-lime-300 p-8 xsm:text-2xl md:text-4xl lg:order-3"
+        data-aos="fade-left"
+      >
         Do you think I could fit in the team? Let's make it reality. Message me
         now!
       </div>
 
-      <div className="row-span-2 ml-2 max-w-sm border-l-4 border-l-lime-300 p-5 xsm:text-2xl lg:order-2">
+      <div
+        className="row-span-2 ml-2 max-w-sm border-l-4 border-l-lime-300 p-5 xsm:text-2xl lg:order-2"
+        data-aos="fade-left"
+      >
         <div className="max-w-sm p-3">
           <div>Email</div>
           <div className="max-w-sm pr-2 xsm:grid sm:flex">
