71f7b5e3f0a31f059f51bf2d39c104da43bfe1f4
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-02-01 17:04:01 +0100

    Sets text styles and md screen grid layout in home section

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,7 +2,7 @@
 
 export function Home() {
   return (
-    <div className="grid gap-2 bg-gradient-to-b from-indigo-700 via-indigo-600 to-secondary xsm:grid-cols-1 xsm:p-4 md:grid-cols-4 md:p-16">
+    <div className="grid gap-2 bg-gradient-to-b from-indigo-700 via-indigo-600 to-secondary xsm:grid-cols-1 xsm:p-4 md:grid-cols-2 md:p-16 lg:grid-cols-4">
       <div className="col-span-2 md:order-1">
         <div className="bg-gradient-to-b from-blue-300 via-lime-200 to-blue-400 bg-clip-text pb-6 text-6xl font-extrabold text-transparent">
           Krzysztof Snopek
@@ -13,7 +13,7 @@ export function Home() {
             I like to create solid apps and learn new technologies.
           </div>
 
-          <div className="text-l order-2 bg-gradient-to-b from-gray-900 via-gray-800 to-indigo-900 bg-clip-text font-extrabold text-transparent xsm:col-span-2 md:col-span-1">
+          <div className="text-l order-2 bg-gradient-to-b bg-clip-text font-extrabold text-transparent xsm:col-span-2 xsm:from-gray-900 xsm:to-gray-900 md:col-span-1 md:from-gray-900 md:via-gray-800 md:to-indigo-900">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus rem
             numquam nihil asperiores maxime recusandae aut placeat.
             Exercitationem officia minima nesciunt blanditiis omnis cumque
